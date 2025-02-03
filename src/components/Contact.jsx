87f8a3a05ec2ee6a7a4f4cb92fb45b147fe522d4
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-02-03 23:04:08 +0100

    Refactor Contact component to update text size and content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,16 +3,16 @@ import { Button } from "../components/ui/button";
 const Contact = () => {
 	return (
 		<section
-			className="py-16 sm:py-24 bg-gray-800 grid-background px-4 sm:px-6 lg:px-8"
+			className="py-16 sm:py-24 bg-gray-800 grid-background px-4 sm:px-6 lg:px-8 "
 			id="contact"
 		>
-			<div className="max-w-7xl mx-auto bg-gradient-to-br from-darkblue to-primary p-8 sm:p-16 lg:p-20 rounded-2xl grid grid-rows-1 sm:grid-rows-2 gap-14 sm:gap-28">
+			<div className="max-w-7xl mx-auto bg-gradient-to-br from-darkblue to-primary p-9 sm:p-16 lg:p-20 rounded-2xl grid grid-rows-1 sm:grid-rows-2 gap-28 sm:gap-60">
 				<h2 className="font-clash-display text-2xl sm:text-3xl">
 					Get in Touch
 				</h2>
 
-				<div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-6 sm:mt-10">
-					<p className="text-xs sm:text-base text-grayLight max-w-lg">
+				<div className="flex flex-col sm:flex-row gap-6 justify-between items-start sm:items-center">
+					<p className="text-base sm:text-base text-grayLight max-w-lg">
 						Whether you want to collaborate or just chat about your next big
 						idea, I&apos;m just an email away. Let&apos;s connect!
 					</p>
