537110f55563d6264ec0f22c8b85772da90df5f8
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-02-03 18:06:01 +0100

    Refactor testimonialdata.js and custom.css

--- src/data/testimonialdata.js
+++ src/data/testimonialdata.js
@@ -8,4 +8,32 @@ export const testimonials = [
 		feedback:
 			"Working with Dolapo for our brand was seamless. Responses were fast, and he was quick to deliver. His attention to details and understanding of what we wanted to achieve, made working with him very easy. His delivery is also professional. We have recommended him to some of our top clientele, and hopefully, they get to experience what we have enjoyed.",
 	},
+	{
+		pofileimage: OyinAraoyeImg,
+		name: "Oyin Araoye",
+		position: "CEO @OyinAraoye",
+		feedback:
+			"Working with Dolapo for our brand was seamless. Responses were fast, and he was quick to deliver. His attention to details and understanding of what we wanted to achieve, made working with him very easy. His delivery is also professional. We have recommended him to some of our top clientele, and hopefully, they get to experience what we have enjoyed.",
+	},
+	{
+		pofileimage: OyinAraoyeImg,
+		name: "Oyin Araoye",
+		position: "CEO @OyinAraoye",
+		feedback:
+			"Working with Dolapo for our brand was seamless. Responses were fast, and he was quick to deliver. His attention to details and understanding of what we wanted to achieve, made working with him very easy. His delivery is also professional. We have recommended him to some of our top clientele, and hopefully, they get to experience what we have enjoyed.",
+	},
+	{
+		pofileimage: OyinAraoyeImg,
+		name: "Oyin Araoye",
+		position: "CEO @OyinAraoye",
+		feedback:
+			"Working with Dolapo for our brand was seamless. Responses were fast, and he was quick to deliver. His attention to details and understanding of what we wanted to achieve, made working with him very easy. His delivery is also professional. We have recommended him to some of our top clientele, and hopefully, they get to experience what we have enjoyed.",
+	},
+	{
+		pofileimage: OyinAraoyeImg,
+		name: "Oyin Araoye",
+		position: "CEO @OyinAraoye",
+		feedback:
+			"Working with Dolapo for our brand was seamless. Responses were fast, and he was quick to deliver. His attention to details and understanding of what we wanted to achieve, made working with him very easy. His delivery is also professional. We have recommended him to some of our top clientele, and hopefully, they get to experience what we have enjoyed.",
+	},
 ];
