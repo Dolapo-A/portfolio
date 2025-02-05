2d2e65940b891cc546db1ef9d77bfc6c5b1e1b41
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-02-05 12:39:09 +0100

    Refactor TestimonialCard component styles

--- src/UI/TestimonialCard.jsx
+++ src/UI/TestimonialCard.jsx
@@ -4,8 +4,8 @@ import { Card, CardContent } from "@/components/ui/card";
 const TestimonialCard = ({ name, feedback, image, position }) => {
 	return (
 		<div className="p-0" data-aos="zoom-out-up">
-			<Card className="max-w-sm mx-auto rounded-2xl">
-				<CardContent className="aspect-auto items-center justify-center p-6 rounded-2xl  card-gradient">
+			<Card className="max-w-sm mx-auto rounded-2xl border border-darkGray  card-gradient">
+				<CardContent className="aspect-auto items-center justify-center p-6 rounded-2xl  ">
 					<p className="text-sm/6 text-gray">{feedback}</p>
 					<div className="border-b border-gray border-dashed py-4"></div>
 					<div className="flex gap-x-7 mt-4">
@@ -15,7 +15,7 @@ const TestimonialCard = ({ name, feedback, image, position }) => {
 							alt={`${name}'s image`}
 						/>
 						<div className="flex flex-col justify-between">
-							<p className="text-lg">{name}</p>
+							<p className="text-lg text-gray">{name}</p>
 							<p className="text-xs text-gray">{position}</p>
 						</div>
 					</div>
