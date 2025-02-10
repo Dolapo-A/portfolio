49e94efbe75a18cc1a7b667183548ab3974a393b
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-02-10 21:18:01 +0100

    Refactor import statements to use @vercel/speed-insights/react package

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ScrollToTop from "./Scolltotop";
 import Navbar from "./components/Navbar";
 import "aos/dist/aos.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
 	return (
