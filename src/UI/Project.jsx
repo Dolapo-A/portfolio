865e992b1c087896717bbaa42abfda3fd0055321
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-02-13 22:29:39 +0100

    Refactor aspect ratio of project images in Project.jsx

--- src/UI/Project.jsx
+++ src/UI/Project.jsx
@@ -13,7 +13,7 @@ const Project = ({
 	links = [],
 }) => {
 	return (
-		<div className="flex flex-col  lg:flex-row gap-8 max-w-7xl border border-darkGray p-4 rounded-2xl mt-9 bg-background h-screen-80">
+		<div className="flex flex-col  lg:flex-row gap-4 max-w-7xl border border-darkGray p-4 rounded-2xl mt-9 bg-background h-screen-80">
 			<picture>
 				<source srcSet={webpImage} type="image/webp" />
 				<img
